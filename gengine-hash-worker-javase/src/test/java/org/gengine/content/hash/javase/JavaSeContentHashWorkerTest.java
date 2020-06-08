396e6ed053dd48c3b2ce2da4ef55b0be3f726b2a
Chen Geng <32190141+CHEN-INFO1104@users.noreply.github.com>
2020-06-09 00:17:46 +0800

    Changed location of test files

---
+++ gengine-hash-worker-javase/src/test/java/org/gengine/content/hash/javase/JavaSeContentHashWorkerTest.java
@@ -0,0 +1,58 @@
+package org.gengine.content.hash.javase;
+
+import static junit.framework.Assert.*;
+
+import org.gengine.content.ContentReference;
+import org.gengine.content.handler.FileContentReferenceHandlerImpl;
+import org.gengine.content.hash.ContentHashWorker;
+import org.gengine.content.hash.javase.JavaSeContentHashNodeWorker;
+import org.junit.Before;
+import org.junit.Test;
+
+public class JavaSeContentHashWorkerTest
+{
+
+    private static final String EXPECTED_VALUE_MD5 = "fe974bb7f67f392239f077b61d649405";
+    private static final String EXPECTED_VALUE_SHA_256 = "29d834b3b1623e55d88f9f61f53ca30f7f6f6d5233cda4c723a40b0237f577b";
+    private static final String EXPECTED_VALUE_SHA_512 = "8f65c82745b7b54bcb0ad87d7540e82b" +
+            "2616c27c632b9269fa6ce71c91e73b69b466cf356883e18a4993624449209693c429b79677c8713110aff5f8c2ed1aa3";
+
+    private ContentHashWorker worker;
+
+    @Before
+    public void setUp() throws Exception {
+        worker = new JavaSeContentHashNodeWorker();
+        ((JavaSeContentHashNodeWorker) worker).setContentReferenceHandler(
+                new FileContentReferenceHandlerImpl());
+    }
+
+    protected void testHash(String hashAlgorithm, String expectedValue) throws Exception
+    {
+        ContentReference source = new ContentReference(
+                this.getClass().getResource("/quick/quick.mpg").toURI().toString(), "video/mpeg");
+
+        String value = worker.generateHash(source, hashAlgorithm);
+
+        assertNotNull("Hash value was null", value);
+        assertEquals(expectedValue, value);
+    }
+
+    @Test
+    public void testMd5() throws Exception
+    {
+        testHash(ContentHashWorker.HASH_ALGORITHM_MD5, EXPECTED_VALUE_MD5);
+    }
+
+    @Test
+    public void testSha256() throws Exception
+    {
+        testHash(ContentHashWorker.HASH_ALGORITHM_SHA_256, EXPECTED_VALUE_SHA_256);
+    }
+
+    @Test
+    public void testSha512() throws Exception
+    {
+        testHash(ContentHashWorker.HASH_ALGORITHM_SHA_512, EXPECTED_VALUE_SHA_512);
+    }
+
+}
