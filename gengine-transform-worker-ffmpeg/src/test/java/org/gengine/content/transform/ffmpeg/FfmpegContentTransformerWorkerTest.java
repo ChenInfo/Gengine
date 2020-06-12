f046b00909eefc093df75f46fc2531a3e8cb7ae4
Chen Geng <32190141+CHEN-INFO1104@users.noreply.github.com>
2020-06-13 04:40:33 +0800

    Changed location of test files

---
+++ gengine-transform-worker-ffmpeg/src/test/java/org/gengine/content/transform/ffmpeg/FfmpegContentTransformerWorkerTest.java
@@ -0,0 +1,104 @@
+package org.gengine.content.transform.ffmpeg;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.io.File;
+
+import static junit.framework.Assert.*;
+
+import org.cheninfo.service.cmr.repository.TemporalSourceOptions;
+import org.gengine.content.ContentReference;
+import org.gengine.content.handler.FileContentReferenceHandlerImpl;
+import org.gengine.content.transform.ContentTransformerWorker;
+import org.gengine.content.transform.ContentTransformerWorkerProgressReporter;
+import org.gengine.content.transform.ffmpeg.FfmpegContentTransformerWorker;
+import org.gengine.content.transform.options.TransformationOptions;
+import org.gengine.content.transform.options.TransformationOptionsImpl;
+import org.gengine.util.TempFileProvider;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @see org.cheninfo.repo.content.transform.ffmpeg.FfmpegContentTransformerWorker
+ *
+ */
+public class FfmpegContentTransformerWorkerTest
+{
+    private static final Log logger = LogFactory.getLog(FfmpegContentTransformerWorkerTest.class);
+
+    private ContentTransformerWorker transformerWorker;
+
+    @Before
+    public void setUp() throws Exception {
+        transformerWorker = new FfmpegContentTransformerWorker();
+        ((FfmpegContentTransformerWorker) transformerWorker).setContentReferenceHandler(
+                new FileContentReferenceHandlerImpl());
+        ((FfmpegContentTransformerWorker) transformerWorker).init();
+    }
+
+    @Test
+    public void testTrimTransformation() throws Exception
+    {
+        TemporalSourceOptions temporalSourceOptions = new TemporalSourceOptions();
+        temporalSourceOptions.setOffset("00:00:00.5");
+        temporalSourceOptions.setDuration("00:00:00.2");
+        TransformationOptions options = new TransformationOptionsImpl();
+        options.addSourceOptions(temporalSourceOptions);
+
+            String sourceExtension = "mpg";
+            String targetExtension = "mp4";
+
+            File sourceFile = new File(this.getClass().getResource("/quick/quick.mpg").toURI());
+            long origSize = sourceFile.length();
+
+            ContentReference source = new ContentReference(
+                    this.getClass().getResource("/quick/quick.mpg").toURI().toString(), "video/mpeg");
+
+            // make a writer for the target file
+            File targetFile = TempFileProvider.createTempFile(
+                    getClass().getSimpleName() + "_quick_" + sourceExtension + "_",
+                    "." + targetExtension);
+
+            ContentReference target = new ContentReference(
+                    targetFile.toURI().toString(), "video/mp4");
+
+
+            transformerWorker.transform(source, target, options, new LoggingProgressReporterImpl());
+
+            long targetSize = targetFile.length();
+
+            assertTrue("Target file size is zero", targetSize > 0);
+            assertTrue("Trimmed target file size should be less than 1/2 original size of " + origSize +
+                    " but was " + targetSize, targetSize < origSize/2);
+
+    }
+
+    public class LoggingProgressReporterImpl implements ContentTransformerWorkerProgressReporter
+    {
+
+        public void onTransformationStarted()
+        {
+            if (logger.isDebugEnabled())
+            {
+                logger.debug("Starting transformation");
+            }
+        }
+
+        public void onTransformationProgress(float progress)
+        {
+            if (logger.isDebugEnabled())
+            {
+                logger.debug(progress*100 + "% progress on transformation");
+            }
+        }
+
+        public void onTransformationComplete()
+        {
+            if (logger.isDebugEnabled())
+            {
+                logger.debug("Completed transformation");
+            }
+        }
+    }
+}
