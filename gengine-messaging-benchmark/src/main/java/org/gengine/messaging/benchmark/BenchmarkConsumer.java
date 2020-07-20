3a1718c832354e4f5c4e5249ce2a05fef16c6cf0
Chen Geng <32190141+CHEN-INFO1104@users.noreply.github.com>
2020-07-20 15:13:32 +0800

    Improved gengine-messaging-benchmark logging

--- gengine-messaging-benchmark/src/main/java/org/gengine/messaging/benchmark/BenchmarkConsumer.java
+++ gengine-messaging-benchmark/src/main/java/org/gengine/messaging/benchmark/BenchmarkConsumer.java
@@ -1,5 +1,8 @@
 package org.gengine.messaging.benchmark;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import org.gengine.messaging.MessageConsumer;
 
 /**
@@ -7,6 +10,10 @@ import org.gengine.messaging.MessageConsumer;
  */
 public class BenchmarkConsumer implements MessageConsumer
 {
+    private static final Log logger = LogFactory.getLog(BenchmarkConsumer.class);
+
+    protected static final int LOG_AFTER_NUM_MESSAGES = 1000;
+
     private int messageCount = 0;
 
     @Override
@@ -15,6 +22,10 @@ public class BenchmarkConsumer implements MessageConsumer
         if (!((BenchmarkMessage) message).getValue().equals(BenchmarkMessage.DEFAULT_VALUE))
         {
             throw new IllegalArgumentException("Could not verify message");
+        }
+        if (messageCount > 0 && messageCount % LOG_AFTER_NUM_MESSAGES == 0)
+        {
+            logger.debug("Received " + messageCount + " messages...");
         }
         messageCount++;
     }
--- gengine-messaging-benchmark/src/main/java/org/gengine/messaging/benchmark/Bootstrap.java
+++ gengine-messaging-benchmark/src/main/java/org/gengine/messaging/benchmark/Bootstrap.java
@@ -3,6 +3,7 @@ package org.gengine.messaging.benchmark;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import java.text.DecimalFormat;
 import java.util.Date;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -20,7 +21,7 @@ public class Bootstrap
 
     protected static final String DEFAULT_QUEUE = "cheninfo.test.benchmark";
     protected static final String USAGE_MESSAGE = "USAGE: brokerUrl numMessages [queue]";
-    protected static final int LOG_AFTER_SENDING_NUM_MESSAGES = 1000;
+    protected static final String LOG_SEPERATOR = "----------------------------------------------\n";
 
     public static void main(String[] args)
     {
@@ -60,7 +61,7 @@ public class Bootstrap
             }
         }
 
-        logger.debug("Sending " + numMessages + " messages...");
+        logStart(numMessages, brokerUrl, queue);
 
         long start = (new Date()).getTime();
 
@@ -69,14 +70,13 @@ public class Bootstrap
         {
             BenchmarkMessage message = BenchmarkMessage.createInstance();
             endpoint.send(message);
-            if (i > 0 && i % LOG_AFTER_SENDING_NUM_MESSAGES == 0)
+            if (i > 0 && i % BenchmarkConsumer.LOG_AFTER_NUM_MESSAGES == 0)
             {
                 logger.debug("Sent " + i + " messages...");
             }
         }
         long endSend = (new Date()).getTime();
         long sendTime = endSend - start;
-        logger.info("Sent " + numMessages + " messages in " + sendTime + "ms");
 
         // Wait for consumer to dequeue all messages
         while (messageConsumer.getMessageCount() < numMessages)
@@ -91,12 +91,54 @@ public class Bootstrap
         }
         long end = (new Date()).getTime();
         long receiveTime = end - start;
-        double messagesPerSecond = numMessages / (receiveTime / 1000.0);
 
-
-        logger.info("Processed " + numMessages + " messages in " + receiveTime + "ms. Throughput: "
-                + "" + Math.round(messagesPerSecond) + " messages/second");
+        logStatistics(numMessages, sendTime, receiveTime);
         System.exit(0);
+    }
+
+    protected static void logStart(int numMessages, String brokerUrl, String queue)
+    {
+        System.out.println("\n\n"
+                + LOG_SEPERATOR
+                + "BENCHMARK START\n"
+                + LOG_SEPERATOR
+                + "Simultaneously sending and receiving..." + "\n\n"
+                + "Number of messages: " + numMessages + "\n"
+                + "Broker URL:         " + brokerUrl + "\n"
+                + "Queue:              "+ queue + "\n"
+                + LOG_SEPERATOR);
+    }
+
+    protected static void logStatistics(int numMessages, long sendTime, long receiveTime)
+    {
+        double messagesPerSecond = numMessages / (receiveTime / 1000.0);
+        System.out.println("\n"
+                + LOG_SEPERATOR
+                + "BENCHMARK RESULTS\n"
+                + LOG_SEPERATOR
+                + "Sent:       " + numMessages + " messages in " + formatMillis(sendTime) + "\n"
+                + "Received:   " + numMessages + " messages in " + formatMillis(receiveTime) + "\n"
+                + "Throughput: " + Math.round(messagesPerSecond) + " messages/second\n"
+                + LOG_SEPERATOR + "\n"
+                + "Note that results include time taken for factors\n"
+                + "like marshalling/unmarshalling of messages, network\n"
+                + "latency, etc., and is not a direct measure of the\n"
+                + "broker's performance.\n");
+    }
+
+    protected static String formatMillis(long milliseconds)
+    {
+        String timeString = null;
+        if (milliseconds > 1000)
+        {
+            DecimalFormat df = new DecimalFormat("#.#");
+            timeString = df.format(milliseconds / 1000.0) + " seconds";
+        }
+        else
+        {
+            timeString = milliseconds  + "ms";
+        }
+        return timeString;
     }
 
 }
