2693b149b60a3b534cbfc4d099ca3816b8186063
Chen Geng <32190141+CHEN-INFO1104@users.noreply.github.com>
2022-07-14 09:11:38 +0800

    Fix Gengine after AWS SDK update

--- gengine-content-handlers/gengine-content-handler-s3/src/main/java/org/gengine/content/handler/s3/S3ContentReferenceHandlerImpl.java
+++ gengine-content-handlers/gengine-content-handler-s3/src/main/java/org/gengine/content/handler/s3/S3ContentReferenceHandlerImpl.java
@@ -1,6 +1,8 @@
 package org.gengine.content.handler.s3;
 
+import java.io.File;
 import java.io.InputStream;
+import java.net.URISyntaxException;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -10,15 +12,19 @@ import org.gengine.content.handler.AbstractUrlContentReferenceHandler;
 
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.GetObjectMetadataRequest;
 import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.transfer.TransferManager;
+import com.amazonaws.services.s3.transfer.TransferManagerBuilder;
+import com.amazonaws.services.s3.transfer.Upload;
 
 /**
  * AWS S3 content handler implementation
@@ -35,11 +41,26 @@ public class S3ContentReferenceHandlerImpl extends AbstractUrlContentReferenceHa
     public static final String HTTPS_PROTOCOL = "https";
 
     private AmazonS3 s3;
+    private TransferManager tm;
 
+    //
+    // If s3AccessKey / s3SecretKey are not overridden
+    // then use DefaultAWSCredentialsProviderChain which searches for credentials in this order:
+    //
+    // - Environment Variables - AWS_ACCESS_KEY_ID and AWS_SECRET_KEY
+    // - Java System Properties - aws.accessKeyId and aws.secretKey
+    // - Credential profiles file at the default location (~/.aws/credentials) shared by all AWS SDKs and the AWS CLI
+    // - Credentials delivered through the Amazon EC2 container service if AWS_CONTAINER_CREDENTIALS_RELATIVE_URI env var is set
+    //   and security manager has permission to access the var,
+    // - Instance profile credentials delivered through the Amazon EC2 metadata service
+    //
     private String s3AccessKey;
     private String s3SecretKey;
+
     private String s3BucketName;
     private String s3BucketRegion;
+
+    private static final String DEFAULT_BUCKET_REGION = "us-east-1";
 
     public void setS3AccessKey(String s3AccessKey)
     {
@@ -66,42 +87,85 @@ public class S3ContentReferenceHandlerImpl extends AbstractUrlContentReferenceHa
         this.s3BucketRegion = s3BucketLocation;
     }
 
+    // Instantiate S3 Service and get or create necessary bucket
     public void init()
     {
-        // Instantiate S3 Service and get or create necessary bucket.
         try
         {
-            s3 = new AmazonS3Client(new BasicAWSCredentials(s3AccessKey, s3SecretKey));
+            s3 = initClient(s3AccessKey, s3SecretKey, s3BucketRegion);
 
-            if (s3BucketRegion != null)
+            if (!s3.doesBucketExist(s3BucketName))
             {
-                if (!s3.doesBucketExist(s3BucketName))
-                {
-                    s3.createBucket(s3BucketName, s3BucketRegion);
-                }
-            }
-            else
-            {
-                s3.createBucket(s3BucketName); // default region
+                // relies on s3ClientBuilder regional endpoint (or else default region)
+                s3.createBucket(s3BucketName);
             }
 
             if (logger.isDebugEnabled())
             {
                 logger.debug("S3 content transport initialization complete: " +
-                        "{ bucketName: '"+s3BucketName+"', bucketLocation: '"+s3BucketRegion + "' }");
+                        "{ bucketName: '" + s3BucketName + "', bucketLocation: '" + s3BucketRegion + "' }");
             }
+
+            tm = TransferManagerBuilder.standard().withS3Client(s3).build();
+
             this.isAvailable = true;
         }
         catch (AmazonClientException e)
         {
-            if (logger.isDebugEnabled())
+            if (logger.isErrorEnabled())
             {
-                logger.debug("S3 content transport failed to initialize bucket " +
+                logger.error("S3 content transport failed to initialize bucket " +
                         "'" + s3BucketName + "': " + e.getMessage());
             }
 
             this.isAvailable = false;
         }
+    }
+
+    // helper
+    public static AmazonS3 initClient(String s3AccessKey, String s3SecretKey, String s3BucketRegion)
+    {
+        // equivalent to "defaultClient" (unless credentials &/or region are overridden)
+        AmazonS3ClientBuilder s3ClientBuilder = AmazonS3ClientBuilder.standard();
+
+        if ((s3AccessKey != null) || (s3SecretKey != null))
+        {
+            // override default credentials
+            s3ClientBuilder.withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(s3AccessKey, s3SecretKey)));
+        }
+
+        if (s3BucketRegion != null)
+        {
+            // override default region
+            s3ClientBuilder.withRegion(s3BucketRegion);
+        }
+
+        AmazonS3 s3 = null;
+
+        try
+        {
+            s3 = s3ClientBuilder.build();
+        }
+        catch (IllegalArgumentException e)
+        {
+            if (e.getCause() != null &&
+                e.getCause() instanceof URISyntaxException &&
+                e.getCause().getMessage().startsWith("Illegal character in authority at index 8: https://s3."))
+            {
+                s3ClientBuilder.withRegion(DEFAULT_BUCKET_REGION);
+                s3 = s3ClientBuilder.build();
+            }
+        }
+        catch (AmazonClientException e)
+        {
+            if (e.getMessage().contains("Unable to find a region via the region provider chain"))
+            {
+                s3ClientBuilder.withRegion(DEFAULT_BUCKET_REGION);
+                s3 = s3ClientBuilder.build();
+            }
+        }
+
+        return s3;
     }
 
     @Override
@@ -157,6 +221,7 @@ public class S3ContentReferenceHandlerImpl extends AbstractUrlContentReferenceHa
         {
             return false;
         }
+
         try
         {
             String s3Url = getS3UrlFromHttpUrl(contentReference.getUri());
@@ -165,17 +230,26 @@ public class S3ContentReferenceHandlerImpl extends AbstractUrlContentReferenceHa
             {
                 logger.debug("Checking existence of reference: " + s3Url);
             }
-            // Get the object and retrieve the input stream
-            S3Object object = s3.getObject(new GetObjectRequest(s3BucketName, getRelativePath(s3Url)));
-            return object != null;
+
+            return s3.doesObjectExist(s3BucketName, getRelativePath(s3Url));
         }
         catch (AmazonServiceException e)
         {
+        	if (e.getStatusCode() == 404)
+        	{
+        		return false;
+        	}
             throw new ContentIOException("Failed to check existence of content: " + e.getMessage(), e);
         }
         catch (Throwable t)
         {
             // Otherwise don't really care why, just that it doesn't exist
+
+            if (logger.isWarnEnabled())
+            {
+                logger.warn("Ignoring failure to check existence of content: " + t.getMessage());
+            }
+
             return false;
         }
     }
@@ -195,8 +269,15 @@ public class S3ContentReferenceHandlerImpl extends AbstractUrlContentReferenceHa
             {
                 logger.debug("Getting remote input stream for reference: " + s3Url);
             }
+
             // Get the object and retrieve the input stream
             S3Object object = s3.getObject(new GetObjectRequest(s3BucketName, getRelativePath(s3Url)));
+
+            if (logger.isDebugEnabled())
+            {
+                logger.debug("Get object: " + s3Url);
+            }
+
             return object.getObjectContent();
         }
         catch (Throwable t)
@@ -206,24 +287,108 @@ public class S3ContentReferenceHandlerImpl extends AbstractUrlContentReferenceHa
     }
 
     @Override
-    public long putInputStream(InputStream sourceInputStream, ContentReference targetContentReference)
+    public long putFile(File sourceFile, ContentReference targetContentReference)
             throws ContentIOException
     {
-        if (!isContentReferenceSupported(targetContentReference))
+        if (! isContentReferenceSupported(targetContentReference))
         {
             throw new ContentIOException("ContentReference not supported");
         }
+
+        long startTime = System.currentTimeMillis();
 
         String remotePath = getRelativePath(targetContentReference.getUri());
 
         try
         {
-            s3.putObject(new PutObjectRequest(
-                    s3BucketName, remotePath, sourceInputStream, new ObjectMetadata()));
+            long contentLength = sourceFile.length();
+
+            try
+            {
+                Upload xfer = tm.upload(s3BucketName, remotePath, sourceFile);
+                xfer.waitForCompletion();
+            }
+            catch (InterruptedException e)
+            {
+                logger.error("Upload was interrupted: "+e.getMessage());
+
+                // Be a good citizen  and set interrupt flag
+                Thread.currentThread().interrupt();
+
+                throw new ContentIOException("Failed to write content", e);
+            }
+
             ObjectMetadata metadata = s3.getObjectMetadata(
                     new GetObjectMetadataRequest(s3BucketName, remotePath));
-            return metadata.getContentLength();
-        } catch (AmazonClientException e)
+
+            long storedContentLength = metadata.getContentLength();
+
+            if (logger.isWarnEnabled())
+            {
+                if (storedContentLength != contentLength)
+                {
+                    logger.warn("Metadata length differs - expected " + contentLength + ", actual "+ storedContentLength);
+                }
+            }
+
+            if (logger.isDebugEnabled())
+            {
+                logger.debug("Upload file: " + s3BucketName + ":" + remotePath + " (in "+(System.currentTimeMillis()-startTime)+" msecs)");
+            }
+
+            return storedContentLength;
+        }
+        catch (AmazonClientException e)
+        {
+            throw new ContentIOException("Failed to write content", e);
+        }
+    }
+
+    @Override
+    public long putInputStream(InputStream sourceInputStream, ContentReference targetContentReference)
+            throws ContentIOException
+    {
+        if (! isContentReferenceSupported(targetContentReference))
+        {
+            throw new ContentIOException("ContentReference not supported");
+        }
+
+        long startTime = System.currentTimeMillis();
+
+        String remotePath = getRelativePath(targetContentReference.getUri());
+
+        try
+        {
+            Long contentLength = targetContentReference.getSize();
+            ObjectMetadata omd = new ObjectMetadata();
+            if (contentLength != null)
+            {
+                omd.setContentLength(contentLength);
+            }
+
+            s3.putObject(new PutObjectRequest(s3BucketName, remotePath, sourceInputStream, omd));
+
+            ObjectMetadata metadata = s3.getObjectMetadata(
+                    new GetObjectMetadataRequest(s3BucketName, remotePath));
+
+            long storedContentLength = metadata.getContentLength();
+
+            if (logger.isWarnEnabled())
+            {
+                if ((contentLength != null) && (storedContentLength != contentLength))
+                {
+                    logger.warn("Metadata length differs - expected " + contentLength + ", actual "+ storedContentLength);
+                }
+            }
+
+            if (logger.isDebugEnabled())
+            {
+                logger.debug("Put object: " + s3BucketName + ":" + remotePath + " (in "+(System.currentTimeMillis()-startTime)+" msecs)");
+            }
+
+            return storedContentLength;
+        }
+        catch (AmazonClientException e)
         {
             throw new ContentIOException("Failed to write content", e);
         }
@@ -242,11 +407,15 @@ public class S3ContentReferenceHandlerImpl extends AbstractUrlContentReferenceHa
         try
         {
             s3.deleteObject(new DeleteObjectRequest(s3BucketName, remotePath));
-        } catch (AmazonClientException e)
+
+            if (logger.isDebugEnabled())
+            {
+                logger.debug("Deleted object: " + s3BucketName + ":" + remotePath);
+            }
+        }
+        catch (AmazonClientException e)
         {
             throw new ContentIOException("Failed to delete content", e);
         }
-
     }
-
 }
