998baa2c86249eb92fd6e0e2068f515d3d0165ff
Chen Geng <32190141+CHEN-INFO1104@users.noreply.github.com>
2022-08-16 13:45:18 +0800

    Camel and other library upgrades - ACS 7.0 compatibility

--- gengine-messaging/gengine-messaging-camel/src/test/java/org/gengine/messaging/camel/dataformat/JacksonDataFormatTest.java
+++ gengine-messaging/gengine-messaging-camel/src/test/java/org/gengine/messaging/camel/dataformat/JacksonDataFormatTest.java
@@ -10,7 +10,7 @@ import java.util.HashMap;
 import org.apache.camel.Exchange;
 import org.apache.camel.component.jackson.JacksonDataFormat;
 import org.apache.camel.impl.DefaultCamelContext;
-import org.apache.camel.impl.DefaultExchange;
+import org.apache.camel.support.DefaultExchange;
 import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
 import org.gengine.messaging.camel.dataformat.SimplePojo.EnumValue;
 import org.gengine.messaging.jackson.ObjectMapperFactory;
